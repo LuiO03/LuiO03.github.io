77851b344ed02321a0287668463fd48d73900d27
luis <lui.fenixand.1997@gmail.com>
2026-06-18 11:15:36 -0500

    asasf

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luio03.github.io/my_portfolio',
+  // base: process.env.NODE_ENV === 'production'
+    // ? '/my_portfolio'
+    // : '/',
   integrations: [icon()],
 });
